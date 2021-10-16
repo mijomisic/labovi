38b8138564c34b7867d3efdf8e267b267c4f7728
Mijo <mijomisic50@gmail.com>
2021-10-16 21:57:09 +0200

    Added 2nd lab

---
+++ 2.Lab/mod.py
@@ -0,0 +1,37 @@
+from Tools.scripts.treesync import raw_input
+
+
+def sort_dict(a):
+    return sorted(a.items(), key=lambda x: x[1], reverse=True)
+
+
+def assign_dictionary():
+    file_data = raw_input("Enter file name for data: ").strip()
+    a = {}
+    with open(file_data, "r") as f:
+        for line in f:
+            (key, val) = line.split()
+            if key in a:
+                a[key] += int(val)
+            else:
+                a[key] = int(val)
+    return a
+
+
+def main():
+    d = {}
+    if raw_input("Is it for labs; [y]es [n]o? ").strip() == 'y':
+        with open('data.txt', "r") as f:
+            for line in f:
+                (key, val) = line.split()
+                if key in d:
+                    d[key] += int(val)
+                else:
+                    d[key] = int(val)
+    else:
+        d = assign_dictionary()
+    print(sort_dict(d))
+
+
+if __name__ == "__main__":
+    main()
